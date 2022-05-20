298353efc407ba615c4315ef481e00997ac65ae1
s.granzow <s.granzow@planhero.de>
2022-05-20 06:33:02 +0200

    start project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chatify/Pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:file_picker/file_picker.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: SplashPage(
+          key: UniqueKey(),
+          onItitializationComplete: () {},
+        ));
+  }
+}
